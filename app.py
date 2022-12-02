cb820cd631fbf406fa898504d48b39465cad6d77
Pranjali Dumbre <74771603+Pranjali693@users.noreply.github.com>
2022-12-02 10:22:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,18 +8,6 @@ app = Flask(__name__)
 def welcome():
     return render_template('form.html')
 
-def add(x,y):
-	return x+y
-
-def subtract(x,y):
-	return x-y
-
-def multiply(x,y):
-	return x*y
-
-def divide(x,y):
-	return x/y
-
 @app.route('/', methods=['POST'])
 def result():
     var_1 = request.form.get("var_1", type=int, default=0)
